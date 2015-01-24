5a1e2657b5ca49c835870e3ac0a1dc2ac2711dec
Andreas Esau <andreasesau@gmail.com>
2015-01-24 07:27:36 +0100

    Commit for Tutorial 03

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -1,26 +1,50 @@
 
 extends RigidBody2D
 
+var input_states = preload("res://scripts/input_states.gd")
+
 export var player_speed = 200
+export var jumpforce = 2000
+export var acceleration = 5
+export var extra_gravity = 400
 
-var btn_right = Input.is_action_pressed("btn_right")
-var btn_left = Input.is_action_pressed("btn_left")
+var raycast_down = null
 
+var current_speed = Vector2(0,0)
 
+var btn_right = input_states.new("btn_right")
+var btn_left = input_states.new("btn_left")
+var btn_jump = input_states.new("btn_jump")
+
+func move(speed, acc, delta):
+	current_speed.x = lerp(current_speed.x , speed, acc * delta)
+	set_linear_velocity(Vector2(current_speed.x,get_linear_velocity().y))
+
+func is_on_ground():
+	if raycast_down.is_colliding():
+		return true
+	else:
+		return false
 
 func _ready():
+	raycast_down = get_node("RayCast2D")
+	raycast_down.add_exception(self)
+
 	# Initalization here
 	set_fixed_process(true)
+	set_applied_force(Vector2(0,extra_gravity))
 	
 func _fixed_process(delta):
-	btn_right = Input.is_action_pressed("btn_right")
-	btn_left = Input.is_action_pressed("btn_left")
 	
-	if btn_left:
-		set_linear_velocity(Vector2(-player_speed,get_linear_velocity().y))
-	elif btn_right:
-		set_linear_velocity(Vector2(player_speed,get_linear_velocity().y))
+	if btn_left.check() == 2:
+		move(-player_speed, acceleration, delta)
+	elif btn_right.check() == 2:
+		move(player_speed, acceleration, delta)
 	else:
-		set_linear_velocity(Vector2(0,get_linear_velocity().y))
-
+		move(0, acceleration, delta)
+	
+	if is_on_ground():
+		if btn_jump.check() == 1:
+			set_axis_velocity(Vector2(0,-jumpforce))
+		
 
