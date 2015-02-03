3558b49e858b9d779fee52d73c6c755ec959d7ec
Andreas Esau <andreasesau@gmail.com>
2015-02-03 21:32:09 +0100

    Commit for Tutorial 09

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -34,6 +34,12 @@ var btn_right = input_states.new("btn_right")
 var btn_left = input_states.new("btn_left")
 var btn_jump = input_states.new("btn_jump")
 
+var anim_player = null
+var anim = ""
+var anim_new = ""
+var anim_speed = 1.0
+var anim_blend = 0.2
+
 func move(speed, acc, delta):
 	current_speed.x = lerp(current_speed.x , speed, acc * delta)
 	set_linear_velocity(Vector2(current_speed.x,get_linear_velocity().y))
@@ -52,7 +58,9 @@ func _ready():
 	# Initalization here
 	set_fixed_process(true)
 	set_applied_force(Vector2(0,extra_gravity))
-
+	
+	
+	anim_player = get_node("rotate/character_sprites/AnimationPlayer")
 
 func rotate_behavior():
 	if ORIENTATION == "right" and ORIENTATION_NEXT == "left":
@@ -76,17 +84,31 @@ func _fixed_process(delta):
 	elif PLAYERSTATE == "air":
 		air_state(delta)
 		
-		
+	if anim != anim_new:
+		anim_new = anim
+		anim_player.play(anim,anim_blend,anim_speed)
+			
 func ground_state(delta):
 
 	if btn_left.check() == 2:
 		move(-player_speed, acceleration, delta)
 		ORIENTATION_NEXT = "left"
+		anim = "run"
+		anim_speed = 2.0
+		anim_blend = 0.2
 	elif btn_right.check() == 2:
 		move(player_speed, acceleration, delta)
 		ORIENTATION_NEXT = "right"
+		anim = "run"
+		anim_speed = 2.0
+		anim_blend = 0.2
 	else:
 		move(0, acceleration, delta)
+		anim = "idle"
+		anim_speed = .5
+		anim_blend = 0.2
+	
+	
 	
 	rotate_behavior()
 	
@@ -113,6 +135,14 @@ func air_state(delta):
 	if btn_jump.check() == 1 and jumping == 1:
 		set_axis_velocity(Vector2(0,-jumpforce))
 		jumping += 1
+	
+	if get_linear_velocity().y > 0:
+		anim = "jump_down"
+		anim_speed = 1.0
+	else:
+		anim = "jump_up"
+		anim_speed = 1.0
+		anim_blend = 0.0
 	
 	rotate_behavior()
 	if is_on_ground():
